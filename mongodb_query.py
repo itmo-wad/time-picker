d2f39164636bdea7ead75d58f6acb1dc319ffea5
TheMac <mulin.pasha@gmail.com>
2020-05-08 14:12:57 +0300

    dirty scelet of project

---
+++ mongodb_query.py
@@ -0,0 +1,46 @@
+import time_picker
+
+
+
+def user_exist(login, password):
+    user = list(time_picker.mongo.db.users_table.find({"username":login.lower(), "password":password}).limit(1))
+    if user:
+        return True
+    else:
+        return False
+
+
+def create_user(login, password, name, surname, phone_number):
+    user = list(time_picker.mongo.db.users_table.find({"username":login.lower()}).limit(1))
+    if user:
+        return False
+    else:
+        time_picker.mongo.db.users_table.insert({"username":login.lower(),"password":password, "name":name, "surname":surname, "phone_number":phone_number})
+        return True
+
+
+#returns list [username, password, name, surname, phone_number]
+def showUser_info(login):
+    user = list(time_picker.mongo.db.users_table.find({"username":login.lower()}).limit(1))
+    return user
+
+
+def showAllusers_table():
+    users_table = time_picker.mongo.db.users_table.find({})
+    return users_table
+
+
+def change_pass(username, old_password, new_password):
+    if user_exist(username, old_password):
+        time_picker.mongo.db.users_table.update({"username":username.lower()},{"$set":{"username":username.lower(),"password":new_password}})
+        return True
+    else:
+        return False
+
+def change_info(username, name, surname, phone_num):
+    user = list(time_picker.mongo.db.users_table.find({"username":username.lower()}).limit(1))
+    if user:
+        time_picker.mongo.db.users_table.update({"username":username.lower()},{"$set":{"username":username.lower(),"name":name, "surname":surname, "phone_number":phone_num}})
+        return True
+    else:
+        return False
