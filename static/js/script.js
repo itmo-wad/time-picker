9407b71f7de6cb09d1d84b503fa2580fa7a3ed54
TheMac <mulin.pasha@gmail.com>
2020-06-06 00:48:26 +0300

    time_slots

--- static/js/script.js
+++ static/js/script.js
@@ -155,3 +155,107 @@ function hideMap(){
 
         }});
       }
+
+
+
+//STEP 4/4 date registration
+
+function init_timeslots(time_slot) {
+
+	let button_slot = document.createElement('button');
+	button_slot.className = "btn btn-outline-secondary btn-block"
+	button_slot.innerText = time_slot;
+	button_slot.id = "not_selected"
+
+	var divslot = document.getElementById("timeslots");
+
+	divslot.appendChild(button_slot);
+}
+
+
+function showslots() {
+
+	var divslot = document.getElementById("timeslots");
+	var call_button = document.getElementById("pick_slot_but");
+	if (call_button.textContent == "Clear slots") {
+		call_button.textContent = "Show slots";
+		while (divslot.firstChild) {
+    	divslot.removeChild(divslot.lastChild);
+  	}
+		return;
+	}
+	call_button.textContent = "Clear slots";
+	//var date = $("#scheduleDate").datepicker({ dateFormat: 'dd,MM,yyyy' }).val();
+	//console.log(date);
+	var range_time = document.getElementById("range_time").value;
+	var start_time = document.getElementById("start_time").value.split(" ")[0];
+	var end_time = document.getElementById("end_time").value.split(" ")[0];
+
+	var start_hour = start_time.split(":")[0];
+	var start_minutes = start_time.split(":")[1];
+
+	var end_hour = end_time.split(":")[0];
+	var end_minutes = end_time.split(":")[1];
+
+
+	var range_hour = range_time.split(":")[0];
+	var range_minutes = range_time.split(":")[1];
+
+	var duration_in_minutes = (parseInt(end_hour)-parseInt(start_hour))*60+(parseInt(end_minutes)-parseInt(start_minutes))
+
+	var range_time_in_minutes = parseInt(range_hour)*60+parseInt(range_minutes)
+
+	var count_slots = Math.ceil(duration_in_minutes/range_time_in_minutes);
+
+	var end_minute_slot = parseInt(start_minutes);
+	var end_hour_slot = parseInt(start_hour);
+	var time_slot = "00:00-00:00";
+  for (var slot = 0; slot < count_slots; slot++) {
+		console.log(start_hour);
+		console.log(range_hour);
+		console.log(parseInt(start_hour)+parseInt(range_hour));
+		console.log((parseInt(start_minutes)+parseInt(range_minutes)).toString());
+		end_minute_slot += parseInt(range_minutes);
+		if (end_minute_slot >= 60) {
+			end_minute_slot -= 60;
+			end_hour_slot += 1;
+			end_hour_slot += parseInt(range_hour);
+
+			time_slot = start_hour+":"+start_minutes+"-"+end_hour_slot.toString()+":"+end_minute_slot.toString();
+
+			init_timeslots(time_slot);
+			console.log(time_slot);
+
+			start_hour = end_hour_slot.toString();
+			start_minutes = end_minute_slot.toString();
+		} else {
+			end_hour_slot += parseInt(range_hour);
+
+			time_slot = start_hour+":"+start_minutes+"-"+end_hour_slot.toString()+":"+end_minute_slot.toString();
+
+			init_timeslots(time_slot);
+			console.log(time_slot);
+
+			start_hour = end_hour_slot.toString();
+			start_minutes = end_minute_slot.toString();
+		}
+	}
+
+}
+
+
+function select_time(selected_time){
+	console.log(selected_time.textContent);
+	if (selected_time.id == "not_selected"){
+		selected_time.id = "selected";
+		selected_time.className = "btn btn-outline-success";
+	} else {
+		selected_time.id = "not_selected";
+		selected_time.className = "btn btn-outline-secondary btn-block";
+	}
+}
+
+function saveWorkHours(){
+	console.log("saving");
+	var work_hours = document.getElementById("timeslots")
+}
